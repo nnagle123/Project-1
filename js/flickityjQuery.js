74b5dcc682e827c4c7f42e1ea882447c40154b56
nnalge123 <nathannagle1@gmail.com>
2019-06-05 14:37:27 -0400

    added Media Queries, styling, added JS and JQuery otherwise finished project

---
+++ js/flickityjQuery.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    console.log("READY")
+    $('.main-carousel').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true
+    });
+});
--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(){
-
-    //all js content in here// 
-
-)};
+function verifyEmail(){
+    var status = false;     
+    var emailRegEx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+         if (document.myform.email1.value.search(emailRegEx) == -1) {
+              alert("Please enter a valid email address.");
+         }
+        
+         else {
+              alert("Woohoo!  Thank you for Subscribing.");
+              status = true;
+         }
+         return status;
+    }       
